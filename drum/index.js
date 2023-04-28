967e5fd4b1b235678621679d264a7668b45c9aff
Ruickof <129295544+Ruickof@users.noreply.github.com>
2023-04-29 01:07:54 +0300

    Add files via upload

--- drum/index.js
+++ drum/index.js
@@ -6,6 +6,8 @@ for (var i = 0; i < drumSayisi; i++) {
     var buttonInnerHTML = this.innerHTML;
 
     sesle(buttonInnerHTML);
+    butonAnimasyon(buttonInnerHTML)
+    
   });
 }
 
@@ -13,45 +15,59 @@ for (var i = 0; i < drumSayisi; i++) {
 
 document.addEventListener("keydown", function (event) {
   sesle(event.key);
+  butonAnimasyon(event.key)
 });
 
 function sesle(key) {
   switch (key) {
-    case "w":
+    case "q":
       var a1 = new Audio("sounds/tom-1.mp3");
       a1.play();
       break;
 
-    case "a":
+    case "w":
       var a2 = new Audio("sounds/tom-2.mp3");
       a2.play();
       break;
 
-    case "s":
+    case "e":
       var a3 = new Audio("sounds/tom-3.mp3");
       a3.play();
       break;
 
-    case "d":
+    case "r":
       var a4 = new Audio("sounds/tom-4.mp3");
       a4.play();
       break;
 
-    case "j":
+    case "t":
       var a5 = new Audio("sounds/crash.mp3");
       a5.play();
       break;
 
-    case "k":
+    case "y":
       var a6 = new Audio("sounds/kick-bass.mp3");
       a6.play();
       break;
+     
+    case "x":
+        var a6 = new Audio("sounds/rick.mp3");
+        a6.play();
+        break; 
 
-    case "l":
+    case "u":
       var a7 = new Audio("sounds/snare.mp3");
       a7.play();
       break;
     default:
       break;
   }
+}
+
+function butonAnimasyon (currentkey){
+    var activeButon = document.querySelector("."+currentkey);
+    activeButon.classList.add("pressed");
+    setTimeout(function(){
+      activeButon.classList.remove("pressed")
+    },150);
 }
