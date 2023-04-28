feca76d4e25eab14e12f37a76ede07a5a1193387
Ruickof <129295544+Ruickof@users.noreply.github.com>
2023-04-28 23:25:07 +0300

    Add files via upload

---
+++ drum/index.js
@@ -0,0 +1,57 @@
+// basılan tuş takibi
+var drumSayisi = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < drumSayisi; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+
+    sesle(buttonInnerHTML);
+  });
+}
+
+// keyboard takibi
+
+document.addEventListener("keydown", function (event) {
+  sesle(event.key);
+});
+
+function sesle(key) {
+  switch (key) {
+    case "w":
+      var a1 = new Audio("sounds/tom-1.mp3");
+      a1.play();
+      break;
+
+    case "a":
+      var a2 = new Audio("sounds/tom-2.mp3");
+      a2.play();
+      break;
+
+    case "s":
+      var a3 = new Audio("sounds/tom-3.mp3");
+      a3.play();
+      break;
+
+    case "d":
+      var a4 = new Audio("sounds/tom-4.mp3");
+      a4.play();
+      break;
+
+    case "j":
+      var a5 = new Audio("sounds/crash.mp3");
+      a5.play();
+      break;
+
+    case "k":
+      var a6 = new Audio("sounds/kick-bass.mp3");
+      a6.play();
+      break;
+
+    case "l":
+      var a7 = new Audio("sounds/snare.mp3");
+      a7.play();
+      break;
+    default:
+      break;
+  }
+}
